0975eb4f4ca1e841ff3fca8bd437397f855f8967
braylonrieman14 <b-rieman.2@onu.edu>
2024-10-24 13:17:54 -0400

    Add project files.

---
+++ P8-2.cpp
@@ -0,0 +1,25 @@
+/*
+* File: P8.1.cpp
+* Author : Braylon Rieman
+* Date : 10 / 24 / 2024
+*
+* Description : Vowel Counter
+*
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//function prototypes
+int countWords(string str);
+
+int main(void)
+{
+
+}
+
+int countWords(string str)
+{
+
+}
